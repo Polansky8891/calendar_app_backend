f693638f88b69485389fd968c24a01d1d4fab88e
Pol Español Roca <pol8891@gmail.com>
2025-06-04 18:18:56 +0200

    configuración inicial servidor y rutas

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
-
+require('dotenv').config();
 
 // crear el servidor de express
 const app = express();
 
 
+// Directorio público
+app.use( express.static('public') );
+
+//Lectura y parseo del body
+app.use( express.json() );
+
+
 // Rutas
-app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-})
+app.use('/api/auth', require('./routes/auth') );
+
+
 
 
 
 // Escuchar peticiones
-app.listen( 4000, () => {
-    console.log(`Servidor corriendo en puerto ${ 4000 }` );
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }` );
 });
---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+/*
+    Ruter de auth
+    host + /api/auth
+*/
+
+
+const { Router } = require('express');
+const router = Router();
+
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
+
+router.post('/new', crearUsuario);
+
+router.post('/', loginUsuario);
+
+router.get('/renew', revalidarToken);
+
+module.exports = router;
